a715e06b3c16fa40a582c2387196935c76dcbe9d
colaz <zlinxx@foxmail.com>
2019-12-27 18:03:29 +0800

    urlconfig

--- chapter02/server.py
+++ chapter02/server.py
@@ -3,14 +3,17 @@ import time
 from tornado import web
 import tornado
 
+
 class MainHandler(web.RequestHandler):
     async def get(self, *args, **kwargs):
         time.sleep(5)
         self.write("hello world")
 
+
 class MainHandler2(web.RequestHandler):
     async def get(self, *args, **kwargs):
         self.write("hello world2")
+
 
 if __name__ == "__main__":
     app = web.Application([
--- chapter02/urlconfig.py
+++ chapter02/urlconfig.py
@@ -2,15 +2,22 @@ import time
 
 from tornado import web
 import tornado
-
+import tornado.ioloop
 
 
 class MainHandler(web.RequestHandler):
     async def get(self, *args, **kwargs):
         self.write("hello world")
 
+
+class PeopleIdHandler(web.RequestHandler):
+    async def get(self, *args, **kwargs):
+        self.write("用户id:{}".format(id))
+
+
 urls = [
     ("/", MainHandler),
+    ("/people/(\d+)/", PeopleIdHandler)
 ]
 
 if __name__ == "__main__":
